6caf782f806685c7c62ba88ecbb46d8ec1613c8a
Zane Taylor <zane@zanetaylor.me>
2022-09-24 01:40:04 +0000

    Svelte map implementation

---
+++ src/components/Map.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let foo;
+
+	// Values that are passed in as props
+	// are immediately available
+	console.log({ foo });
+</script>
+
+<div id="map" ref="map"></div>
+<div class="overlay"></div>
+<div class="greet" :class="{ active: loc }">
+  <p class="greeting">
+    It looks like you're visiting from <span class="loc">{{ loc }}</span
+    >.
+  </p>
+  <p class="greeting"><span class="hi">How's the weather?</span></p>
+</div>
