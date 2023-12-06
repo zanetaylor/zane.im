9c69f526c4b64bd6f7b24e3435de7f1d1e99ffa6
Zane Taylor <zane@zanetaylor.me>
2023-12-06 16:56:05 +0000

    Add collection and template for misc. pages

--- src/content/content.ts
+++ src/content/content.ts
@@ -12,8 +12,15 @@ const blogCollection = defineCollection({
   }),
 });
 
+const pageCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+  }),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   'blog': blogCollection,
+  'pages': pageCollection
 };
