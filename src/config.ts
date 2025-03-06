aed9470d4eba8524530c80d7eddd146d71008b8e
Zane Taylor <me@zane.im>
2025-03-06 22:42:44 +0000

    Meta properties & color tweaks

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export const SITE = {
     // Your site's detail?
     name: 'Zane Taylor',
-    title: 'Zane Taylor // Creative Web Engineer',
-    description: 'The personal home page of a creative web engineer. He lives in Oregon, building stuff with code, data, maps, and open source.',
+    title: 'Zane Taylor // ',
+    description: 'The personal home page of a creative software engineer. He lives in Oregon, building stuff with code, data, maps, and open source.',
     url: 'https://zane.im',
     githubUrl: 'https://github.com/zanetaylor/zane.im',
     listDrafts: true
